81f1567b6912feb55bd1d8089ebf4a8bf8f748a3
CNLHC <cn_lhc@qq.com>
2019-11-05 17:44:29 +0800

    Setup basic test configuration

---
+++ test/basic.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {getForm}  from '../src/index'
+import renderer from 'react-test-renderer';
+
+test('HelloWorld', () => {
+    const [formAction,Form] = getForm({})
+    const component = renderer.create(
+        <Form  formMeta={[]}/>
+      );
+      expect(component).toMatchSnapshot();
+      console.log(component.toTree())
+});
