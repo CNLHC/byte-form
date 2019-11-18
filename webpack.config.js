19e0f3b5bd7eb07fec18f6a15db1a59a666697d2
CNLHC <cn_lhc@qq.com>
2019-11-18 14:52:35 +0800

    use webpack to bundle things(again)

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,44 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: {
-    bundle: "./src/index.ts"
-  },
-  devtool: "inline-source-map",
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  output: {
-    filename: "byte-form.js",
-    path: path.resolve(__dirname, "dist"),
-    libraryTarget: "commonjs"
-  },
-  optimization: {
-    minimize: false
-  },
-
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "babel-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.js$/,
-        use: ["source-map-loader"],
-        enforce: "pre"
-      }
-    ]
-  },
-  externals: [
-    {
-      react: {
-        commonjs: "react"
-      }
+    entry: {
+        bundle: './src/byte-form.ts',
     },
-    /antd/,
+    devtool: 'inline-source-map',
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'byte-form.js',
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'commonjs',
+    },
+    optimization: {
+        minimize: false,
+    },
 
-    // /react-dom.*/,
-    // /react-redux.*/,
-    // /redux.*/,
-    / immer /
-  ]
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'ts-loader'],
+            },
+            {
+                test: /\.js$/,
+                use: ['source-map-loader'],
+                enforce: 'pre',
+            },
+        ],
+    },
+    externals: [
+        {
+            react: {
+                commonjs: 'react',
+            },
+        },
+        /antd/,
+        / immer /,
+    ],
 };
